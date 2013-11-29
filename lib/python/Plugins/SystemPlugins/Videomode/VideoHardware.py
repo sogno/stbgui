c1aa3b07362f0884703c4d4f1561714518f97ece
sogno <sogno.dev@gmail.com>
2013-11-29 18:44:27 +0100

    Removed Ybprb from Sogno 8800hd

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -85,13 +85,14 @@ class VideoHardware:
 	modes["Scart"] = ["PAL", "NTSC", "Multi"]
 	modes["DVI-PC"] = ["PC"]
 
-	#if  chipset.find('7335') != -1 or chipset.find('7358') != -1 or chipset.find('7356') != -1 or chipset.find('7405') != -1 or chipset.find('7403') != -1:
-	if  chipset.find('7335') != -1 or chipset.find('7358') != -1 or chipset.find('7356') != -1 or chipset.find('7403') != -1:	  
-		modes["YPbPr"] = ["1080i", "1080p", "720p", "576p", "480p", "576i", "480i"]
+	if  chipset.find('7335') != -1 or chipset.find('7358') != -1 or chipset.find('7356') != -1 or chipset.find('7403') != -1:
+		if not about.getHardwareTypeString().lower() == "sogno-8800hd":
+			modes["YPbPr"] = ["1080i", "1080p", "720p", "576p", "480p", "576i", "480i"]
 		modes["DVI"] = ["1080i", "1080p", "720p", "576p", "480p", "576i", "480i"]
 		widescreen_modes = set(["720p", "1080i", "1080p"])
 	else:
-		modes["YPbPr"] = ["720p", "1080i", "576p", "480p", "576i", "480i"]
+		if not about.getHardwareTypeString().lower() == "sogno-8800hd":
+			modes["YPbPr"] = ["720p", "1080i", "576p", "480p", "576i", "480i"]
 		modes["DVI"] = ["720p", "1080i", "576p", "480p", "576i", "480i"]
 		widescreen_modes = set(["720p", "1080i"])
 
