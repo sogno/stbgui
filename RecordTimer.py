fbc478278d743becc93f19560ceae4c23da94eff
sogno <sogno.dev@gmail.com>
2014-07-28 11:40:10 +0200

    Timer fix

--- RecordTimer.py
+++ RecordTimer.py
@@ -741,16 +741,19 @@ class RecordTimer(timer.Timer):
 
 		list.append('</timers>\n')
 
-		file = open(self.Filename + ".writing", "w")
-		for x in list:
-			file.write(x)
-		file.flush()
+		try:
+			file = open(self.Filename + ".writing", "w")
+			for x in list:
+				file.write(x)
+			file.flush()
 
-		import os
-		os.fsync(file.fileno())
-		file.close()
-		os.rename(self.Filename + ".writing", self.Filename)
-
+			import os
+			os.fsync(file.fileno())
+			file.close()
+			os.rename(self.Filename + ".writing", self.Filename)
+		except:
+			pass
+		      
 	def getNextZapTime(self, isWakeup=False):
 		now = time()
 		for timer in self.timer_list:
